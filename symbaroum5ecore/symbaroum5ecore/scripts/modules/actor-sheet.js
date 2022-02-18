30fb420d5f704de176dd358b856862384a0c5065
pwatson100 <paul@skynet2020.co.uk>
2022-02-18 16:45:43 +0000

    Initial Character and NPC sheet styling.

--- symbaroum5ecore/symbaroum5ecore/scripts/modules/actor-sheet.js
+++ symbaroum5ecore/symbaroum5ecore/scripts/modules/actor-sheet.js
@@ -1,10 +1,9 @@
-import { COMMON } from '../common.js'
+import { COMMON } from '../common.js';
 import { logger } from '../logger.js';
-import { Spellcasting } from './spellcasting.js'
-import { ActorSyb5e } from './actor.js'
+import { Spellcasting } from './spellcasting.js';
+import { ActorSyb5e } from './actor.js';
 
 export class SheetCommon {
-
   static NAME = 'SheetCommon';
 
   /** SETUP **/
@@ -36,10 +35,10 @@ export class SheetCommon {
     COMMON[sheetClass.NAME] = {
       scope: 'dnd5e',
       sheetClass,
-    }
+    };
 
     /* need to use our own defaults to set our defaults */
-    COMMON[sheetClass.NAME].id = `${COMMON[sheetClass.NAME].scope}.${COMMON[sheetClass.NAME].sheetClass.name}`
+    COMMON[sheetClass.NAME].id = `${COMMON[sheetClass.NAME].scope}.${COMMON[sheetClass.NAME].sheetClass.name}`;
 
     /* store this information in a better place */
     game.syb5e.sheetClasses.push(COMMON[sheetClass.NAME]);
@@ -52,61 +51,58 @@ export class SheetCommon {
   /* -------------------------------------------- */
 
   static _getCorruptionAbilityData(actor, contextAbilities) {
-
     let defaultEntries = [];
 
     /* if this actor has any spellcasting, allow it to be selected as corruption stat */
     if (actor.data.data.attributes.spellcasting?.length > 0) {
-      defaultEntries.push({ability: 'spellcasting', label: COMMON.localize('DND5E.Spellcasting')});
+      defaultEntries.push({ ability: 'spellcasting', label: COMMON.localize('DND5E.Spellcasting') });
     }
 
-    defaultEntries.push({ability: 'custom', label: COMMON.localize('SYB5E.Corruption.Custom')});
+    defaultEntries.push({ ability: 'custom', label: COMMON.localize('SYB5E.Corruption.Custom') });
 
-    const corruptionAbilities = Object.entries(contextAbilities).reduce( (acc, [key, val]) => {
-      acc.push({ability: key, label: val.label}) 
+    const corruptionAbilities = Object.entries(contextAbilities).reduce((acc, [key, val]) => {
+      acc.push({ ability: key, label: val.label });
       return acc;
-    },defaultEntries);
+    }, defaultEntries);
 
     let corruptionAbilityData = {
       path: game.syb5e.CONFIG.PATHS.corruption.ability,
       abilities: corruptionAbilities,
-      current: actor.corruption.ability
-    }
+      current: actor.corruption.ability,
+    };
 
     /* can only edit max corruption if using a custom value */
     corruptionAbilityData.disabled = corruptionAbilityData.current !== 'custom' ? 'disabled' : '';
 
-    return corruptionAbilityData
+    return corruptionAbilityData;
   }
 
   /* -------------------------------------------- */
 
   /* Common context data between characters and NPCs */
   static _getCommonData(actor, context) {
-    
     /* Add in our corruption values in 'data.attributes' */
     const commonData = {
       sybPaths: game.syb5e.CONFIG.PATHS,
       corruptionAbilities: SheetCommon._getCorruptionAbilityData(actor, context.data.abilities),
       data: {
         attributes: {
-          corruption: actor.corruption
+          corruption: actor.corruption,
         },
         details: {
-          shadow: actor.shadow
-        }
-      }
-    }
+          shadow: actor.shadow,
+        },
+      },
+    };
 
     mergeObject(context, commonData);
   }
 
   static async renderCurrencyRow(actor) {
-   
     const data = {
       currency: actor.data.data.currency,
       labels: game.syb5e.CONFIG.CURRENCY,
-    }
+    };
 
     COMMON.translateObject(data.labels);
 
@@ -117,7 +113,7 @@ export class SheetCommon {
 
   /* -------------------------------------------- */
 
-  static async _render(){
+  static async _render() {
     /* suppress spell slot display */
     this.element.find('.spell-slots').css('display', 'none');
 
@@ -125,20 +121,20 @@ export class SheetCommon {
 
     switch (this.actor.type) {
       case 'character':
-    /* characters have a currency row already that we need to replace */
+        /* characters have a currency row already that we need to replace */
         this.element.find('.currency').replaceWith(currencyRow);
         break;
 
-    /* NPCs have none and we want to put it at the top of features */
+      /* NPCs have none and we want to put it at the top of features */
       case 'npc':
         this.element.find('.features .inventory-filters').prepend(currencyRow);
         break;
     }
 
     //if ( !this.isEditable ) return false;
-    
+
     //currency conversion
-    this.element.find('.currency-convert').click( SheetCommon._onSybCurrencyConvert.bind(this) );
+    this.element.find('.currency-convert').click(SheetCommon._onSybCurrencyConvert.bind(this));
   }
 
   /** \COMMON **/
@@ -150,19 +146,17 @@ export class SheetCommon {
     await this._onSubmit(event);
     return this.actor.convertSybCurrency();
   }
-
 }
 
 export class Syb5eActorSheetCharacter extends COMMON.CLASSES.ActorSheet5eCharacter {
-
-  static NAME = "Syb5eActorSheetCharacter"
+  static NAME = 'Syb5eActorSheetCharacter';
 
   /* -------------------------------------------- */
 
-  static register(){
+  static register() {
     this.defaults();
 
-    /* register our sheet */ 
+    /* register our sheet */
     Actors.registerSheet(COMMON[this.NAME].scope, COMMON[this.NAME].sheetClass, {
       types: ['character'],
       makeDefault: true,
@@ -173,7 +167,7 @@ export class Syb5eActorSheetCharacter extends COMMON.CLASSES.ActorSheet5eCharact
   /* -------------------------------------------- */
 
   static defaults() {
-    SheetCommon.defaults(this); 
+    SheetCommon.defaults(this);
   }
 
   /* -------------------------------------------- */
@@ -184,14 +178,14 @@ export class Syb5eActorSheetCharacter extends COMMON.CLASSES.ActorSheet5eCharact
 
   //TODO expand to other modes (like limited)
   get template() {
-    return `${COMMON.DATA.path}/templates/actors/syb5e-character-sheet.html`
+    return `${COMMON.DATA.path}/templates/actors/syb5e-character-sheet.html`;
   }
 
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      classes: ["syb5e", "dnd5e", "sheet", "actor", "character"],
-      //width: 720,
-      //height: 680
+      classes: ['syb5e', 'dnd5e', 'sheet', 'actor', 'character'],
+      width: 757,
+      height: 749,
     });
   }
 
@@ -218,7 +212,7 @@ export class Syb5eActorSheetCharacter extends COMMON.CLASSES.ActorSheet5eCharact
 
     /* Inject the extended rest button and listener ( TODO should the whole sheet be injected like this?) */
     const footer = this.element.find('.hit-dice .attribute-footer');
-    footer.append(`<a class="rest extended-rest" title="${COMMON.localize('SYB5E.Rest.ExtRest')}">${COMMON.localize('SYB5E.Rest.ExtendedAbbr')}</a>`)
+    footer.append(`<a class="rest extended-rest" title="${COMMON.localize('SYB5E.Rest.ExtRest')}">${COMMON.localize('SYB5E.Rest.ExtendedAbbr')}</a>`);
 
     /* activate listener for Extended Rest Button */
     this.element.find('.extended-rest').click(this._onExtendedRest.bind(this));
@@ -226,7 +220,7 @@ export class Syb5eActorSheetCharacter extends COMMON.CLASSES.ActorSheet5eCharact
 
   /* -------------------------------------------- */
 
-  async _onShortRest(event){
+  async _onShortRest(event) {
     event.preventDefault();
     await this._onSubmit(event);
     return this.actor.shortRest();
@@ -234,7 +228,7 @@ export class Syb5eActorSheetCharacter extends COMMON.CLASSES.ActorSheet5eCharact
 
   /* -------------------------------------------- */
 
-  async _onLongRest(event){
+  async _onLongRest(event) {
     event.preventDefault();
     await this._onSubmit(event);
     return this.actor.longRest();
@@ -257,20 +251,18 @@ export class Syb5eActorSheetCharacter extends COMMON.CLASSES.ActorSheet5eCharact
   }
 
   /* -------------------------------------------- */
-  
 }
 
 export class Syb5eActorSheetNPC extends COMMON.CLASSES.ActorSheet5eNPC {
-
-  static NAME = "Syb5eActorSheetNPC"
+  static NAME = 'Syb5eActorSheetNPC';
 
   /* -------------------------------------------- */
 
-  static register(){
+  static register() {
     this.defaults();
 
-    /* register our sheet */ 
-    Actors.registerSheet("dnd5e", Syb5eActorSheetNPC, {
+    /* register our sheet */
+    Actors.registerSheet('dnd5e', Syb5eActorSheetNPC, {
       types: ['npc'],
       makeDefault: true,
       label: COMMON.localize('SYB5E.Sheet.NPC.Label'),
@@ -291,16 +283,16 @@ export class Syb5eActorSheetNPC extends COMMON.CLASSES.ActorSheet5eNPC {
 
   //TODO expand to other modes (like limited)
   get template() {
-    return `${COMMON.DATA.path}/templates/actors/syb5e-npc-sheet.html`
+    return `${COMMON.DATA.path}/templates/actors/syb5e-npc-sheet.html`;
   }
 
   /* -------------------------------------------- */
 
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      classes: ["syb5e", "dnd5e", "sheet", "actor", "npc"],
-      //width: 720,
-      //height: 680
+      classes: ['syb5e', 'dnd5e', 'sheet', 'actor', 'npc'],
+      width: 625,
+      height: 705,
     });
   }
 
@@ -324,5 +316,4 @@ export class Syb5eActorSheetNPC extends COMMON.CLASSES.ActorSheet5eNPC {
     const boundRender = await SheetCommon._render.bind(this);
     boundRender(...args);
   }
-
 }
