a7c15235902f2e63a0d899d44a5e4e096a8150c7
pwatson100 <paul@skynet2020.co.uk>
2022-02-23 15:00:48 +0000

    Sheet Config options working for all use cases (#22)
    * Actor background and Text Colour selection complete.
    
    * Removed border for default DnD5e sheet config.
    
    * Sheet Config finally woking for all use cases.

--- symbaroum5ecore/symbaroum5ecore/scripts/config.js
+++ symbaroum5ecore/symbaroum5ecore/scripts/config.js
@@ -1,12 +1,11 @@
-import { COMMON } from './common.js'
-import { SybConfigApp } from './modules/apps/config-app.js'
+import { COMMON } from './common.js';
+import { SybConfigApp } from './modules/apps/config-app.js';
 
 /* CONFIG class for syb5e data.
  * Stored in 'game.syb5e.CONFIG'
  */
 export class SYB5E {
-
-  static NAME = "SYB5E_CONFIG";
+  static NAME = 'SYB5E_CONFIG';
 
   static register() {
     this.globals();
@@ -15,7 +14,7 @@ export class SYB5E {
     this.hooks();
   }
 
-  static get CONFIG(){
+  static get CONFIG() {
     return globalThis.game.syb5e.CONFIG;
   }
 
@@ -44,35 +43,21 @@ export class SYB5E {
         type: String,
         config: false,
         scope: 'client',
-        default: 'url(../images/background/bg-green.webp) repeat'
+        default: 'url(../images/background/bg-green.webp) repeat',
+      },
+      charTextColour: {
+        restricted: false,
+        type: String,
+        config: false,
+        scope: 'client',
+        default: '#ffffff',
       },
       npcBGChoice: {
         restricted: false,
         type: String,
         config: false,
         scope: 'client',
-        default: 'url(../images/background/bg-green.webp) repeat'
-      },
-      titleBGChoice: {
-        restricted: false,
-        type: String,
-        config: false,
-        scope: 'client',
-        default: 'url(../images/background/bg-green.webp) repeat'
-      },
-      editableChoice: {
-        restricted: false,
-        type: String,
-        config: false,
-        scope: 'client',
-        default: 'url(../images/background/bg-green.webp) repeat'
-      },
-      nonEditableChoice: {
-        restricted: false,
-        type: String,
-        config: false,
-        scope: 'client',
-        default: 'url(../images/background/bg-green.webp) repeat'
+        default: 'url(../images/background/bg-green.webp) repeat',
       },
       switchCharBGColour: {
         restricted: false,
@@ -88,42 +73,64 @@ export class SYB5E {
         scope: 'client',
         default: 'url(../images/background/bg-red.webp) repeat',
       },
-      switchTitleColour: {
+      npcTextColour: {
         restricted: false,
         type: String,
         config: false,
         scope: 'client',
-        default: 'url(../images/background/bg-green.webp) repeat',
+        default: '#ffffff',
       },
-      switchEditableColour: {
+      fontFamily: {
         restricted: false,
         type: String,
         config: false,
         scope: 'client',
-        default: 'url(../images/background/bg-green.webp) repeat',
+        default: 'Fondamento',
       },
-      switchNonEditableColour: {
+      charBorder: {
         restricted: false,
         type: String,
         config: false,
         scope: 'client',
-        default: 'url(../images/background/bg-green.webp) repeat',
+        default: '13px solid transparent',
       },
-      addMenuButton: {
-        scope: 'world',
-        config: true,
-        default: SybConfigApp.getDefaults.addMenuButton,
-        type: Boolean,
-        onChange: (enabled) => {
-          SybConfigApp.toggleConfigButton(enabled);
-        },
-      }
-    }
+      npcBorder: {
+        restricted: false,
+        type: String,
+        config: false,
+        scope: 'client',
+        default: '13px solid transparent',
+      },
+      charChanged: {
+        restricted: false,
+        type: String,
+        config: false,
+        scope: 'client',
+        default: '',
+      },
+      npcChanged: {
+        restricted: false,
+        type: String,
+        config: false,
+        scope: 'client',
+        default: '',
+      },
+      // addMenuButton: {
+      //   name: 'SYB5E.setting.config-menu-label.name',
+      //   scope: 'world',
+      //   config: true,
+      //   default: SybConfigApp.getDefaults.addMenuButton,
+      //   type: Boolean,
+      //   onChange: (enabled) => {
+      //     SybConfigApp.toggleConfigButton(enabled);
+      //   },
+      // },
+    };
 
     game.settings.registerMenu('symbaroum5ecore', 'symbaroumSettings', {
-      name: 'symbaroum5ecore.OPTIONAL_CONFIG_MENULABEL',
-      label: 'symbaroum5ecore.OPTIONAL_CONFIG_MENULABEL',
-      hint: 'symbaroum5ecore.OPTIONAL_CONFIG_MENUHINT',
+      name: 'SYB5E.setting.config-menu-label.name',
+      label: 'SYB5E.setting.config-menu-label.name',
+      hint: 'SYB5E.setting.config-menu-hint.hint',
       icon: 'fas fa-palette',
       type: SybConfigApp,
       restricted: false,
@@ -136,11 +143,16 @@ export class SYB5E {
     Hooks.once('ready', async () => {
       /* setup config options */
       let r = document.querySelector(':root');
-      await r.style.setProperty('--syb5e-pc-background-image', game.settings.get('symbaroum5ecore', 'switchCharBGColour'));
-      await r.style.setProperty('--syb5e-npc-background-image', game.settings.get('symbaroum5ecore', 'switchNpcBGColour'));
-    })
+      await r.style.setProperty('--syb5e-pc-background-image', COMMON.setting('switchCharBGColour'));
+      await r.style.setProperty('--syb5e-pc-color', COMMON.setting('charTextColour'));
+      await r.style.setProperty('--syb5e-npc-background-image', COMMON.setting('switchNpcBGColour'));
+      await r.style.setProperty('--syb5e-npc-color', COMMON.setting('npcTextColour'));
+      await r.style.setProperty('--syb5e-pc-font', COMMON.setting('fontFamily'));
+      await r.style.setProperty('--syb5e-npc-font', COMMON.setting('fontFamily'));
+      await r.style.setProperty('--syb5e-pc-sheet-border', COMMON.setting('charBorder'));
+      await r.style.setProperty('--syb5e-npc-sheet-border', COMMON.setting('npcBorder'));
+    });
   }
-
 
   /* setting our global config data */
   static globals() {
@@ -158,83 +170,95 @@ export class SYB5E {
       'SYB5E.Level.Seventh',
       'SYB5E.Level.Eighth',
       'SYB5E.Level.Nineth',
-    ]
+    ];
 
     /* 3 rest types for syb */
     globalThis.game.syb5e.CONFIG.REST_TYPES = {
       short: 'short',
       long: 'long',
-      extended: 'ext'
-    }
+      extended: 'ext',
+    };
 
-    globalThis.game.dnd5e.config.limitedUsePeriods.er = COMMON.localize("SYB5E.Rest.Extended");
+    globalThis.game.dnd5e.config.limitedUsePeriods.er = COMMON.localize('SYB5E.Rest.Extended');
 
     /* Add in "Greater Artifact" rarity for items */
-    globalThis.game.dnd5e.config.itemRarity.greaterArtifact = COMMON.localize("SYB5E.Item.Rarity.GreaterArtifact");
+    globalThis.game.dnd5e.config.itemRarity.greaterArtifact = COMMON.localize('SYB5E.Item.Rarity.GreaterArtifact');
 
     /* Add in "Alchemical Weapon" category for weapons */
-    globalThis.game.dnd5e.config.weaponTypes.alchemical = COMMON.localize("SYB5E.Item.Subtype.Alchemical");
+    globalThis.game.dnd5e.config.weaponTypes.alchemical = COMMON.localize('SYB5E.Item.Subtype.Alchemical');
 
     /* Add in "Alchemical" as a weapon proficiency */
-    globalThis.game.dnd5e.config.weaponProficiencies.alc = COMMON.localize("SYB5E.Proficiency.WeaponAlchemical");
+    globalThis.game.dnd5e.config.weaponProficiencies.alc = COMMON.localize('SYB5E.Proficiency.WeaponAlchemical');
 
     /* Map the weapon type key (alchemical) to the proficiency key (alc) */
-    globalThis.game.dnd5e.config.weaponProficienciesMap.alchemical = "alc";
+    globalThis.game.dnd5e.config.weaponProficienciesMap.alchemical = 'alc';
 
     /* Extend dnd5e weapon properties */
-    mergeObject(globalThis.game.dnd5e.config.weaponProperties, COMMON.translateObject({
-      are: "SYB5E.Item.WeaponProps.AreaEffect",
-      bal: "SYB5E.Item.WeaponProps.Balanced",
-      crw: "SYB5E.Item.WeaponProps.Crewed",
-      con: "SYB5E.Item.WeaponProps.Concealed",
-      dim: "SYB5E.Item.WeaponProps.DeepImpact",
-      ens: "SYB5E.Item.WeaponProps.Ensnaring",
-      imm: "SYB5E.Item.WeaponProps.Immobile",
-      msv: "SYB5E.Item.WeaponProps.Massive",
-      res: "SYB5E.Item.WeaponProps.Restraining",
-      sge: "SYB5E.Item.WeaponProps.Siege",
-    }));
+    mergeObject(
+      globalThis.game.dnd5e.config.weaponProperties,
+      COMMON.translateObject({
+        are: 'SYB5E.Item.WeaponProps.AreaEffect',
+        bal: 'SYB5E.Item.WeaponProps.Balanced',
+        crw: 'SYB5E.Item.WeaponProps.Crewed',
+        con: 'SYB5E.Item.WeaponProps.Concealed',
+        dim: 'SYB5E.Item.WeaponProps.DeepImpact',
+        ens: 'SYB5E.Item.WeaponProps.Ensnaring',
+        imm: 'SYB5E.Item.WeaponProps.Immobile',
+        msv: 'SYB5E.Item.WeaponProps.Massive',
+        res: 'SYB5E.Item.WeaponProps.Restraining',
+        sge: 'SYB5E.Item.WeaponProps.Siege',
+      })
+    );
 
     /* extend dnd5e damage types
-     * -> This supports both the damage and healing types 
+     * -> This supports both the damage and healing types
      */
-    mergeObject(globalThis.game.dnd5e.config.damageTypes, COMMON.translateObject({
-      permc: "SYB5E.Corruption.PermDamage",
-      tempc: "SYB5E.Corruption.TempDamage",
-    }));
+    mergeObject(
+      globalThis.game.dnd5e.config.damageTypes,
+      COMMON.translateObject({
+        permc: 'SYB5E.Corruption.PermDamage',
+        tempc: 'SYB5E.Corruption.TempDamage',
+      })
+    );
 
     /* add in "None" spell school (mainly for Troll Singer Songs) */
-    mergeObject(globalThis.game.dnd5e.config.spellSchools, COMMON.translateObject({
-      non: "None"
-    }));
+    mergeObject(
+      globalThis.game.dnd5e.config.spellSchools,
+      COMMON.translateObject({
+        non: 'None',
+      })
+    );
 
     /* Store new armor properties */
     globalThis.game.syb5e.CONFIG.ARMOR_PROPS = COMMON.translateObject({
-      con: "SYB5E.Item.ArmorProps.Concealable",
-      cmb: "SYB5E.Item.ArmorProps.Cumbersome",
-      noi: "SYB5E.Item.ArmorProps.Noisy",
-      wei: "SYB5E.Item.ArmorProps.Weighty"
+      con: 'SYB5E.Item.ArmorProps.Concealable',
+      cmb: 'SYB5E.Item.ArmorProps.Cumbersome',
+      noi: 'SYB5E.Item.ArmorProps.Noisy',
+      wei: 'SYB5E.Item.ArmorProps.Weighty',
     });
 
     /* add 'abomination' and 'phenomenon' to creature types */
-    mergeObject(globalThis.game.dnd5e.config.creatureTypes, COMMON.translateObject({
-      abomination: 'SYB5E.Creature.Abomination',
-      phenomenon: 'SYB5E.Creature.Phenomenon',
-    }));
+    mergeObject(
+      globalThis.game.dnd5e.config.creatureTypes,
+      COMMON.translateObject({
+        abomination: 'SYB5E.Creature.Abomination',
+        phenomenon: 'SYB5E.Creature.Phenomenon',
+      })
+    );
 
     /* Replace currency names */
     globalThis.game.syb5e.CONFIG.CURRENCY = COMMON.translateObject({
-      gp: 'SYB5E.Currency.Thaler', 
-      sp: 'SYB5E.Currency.Shilling', 
-      cp: 'SYB5E.Currency.Orteg'
+      gp: 'SYB5E.Currency.Thaler',
+      sp: 'SYB5E.Currency.Shilling',
+      cp: 'SYB5E.Currency.Orteg',
     });
 
     /* redefine used currencies (only cp, sp, gp) */
     globalThis.game.syb5e.CONFIG.CURRENCY_CONVERSION = {
-      cp: {into: 'sp', each: 10},
-      sp: {into: 'gp', each: 10},
-      gp: {into: 'gp', each: 1}
-    }
+      cp: { into: 'sp', each: 10 },
+      sp: { into: 'gp', each: 10 },
+      gp: { into: 'gp', each: 1 },
+    };
 
     /* The default values for syb5e actor data */
     globalThis.game.syb5e.CONFIG.DEFAULT_FLAGS = {
@@ -243,25 +267,25 @@ export class SYB5E {
         temp: 0,
         permanent: 0,
         value: 0,
-        max: 0
+        max: 0,
       },
       manner: '',
       shadow: '',
-    }
+    };
 
     /* The default values for syb5e item data */
     globalThis.game.syb5e.CONFIG.DEFAULT_ITEM = {
       favored: 0,
-      armorProps: Object.keys(game.syb5e.CONFIG.ARMOR_PROPS).reduce( (acc, key) => {
-        acc[key]=false;
+      armorProps: Object.keys(game.syb5e.CONFIG.ARMOR_PROPS).reduce((acc, key) => {
+        acc[key] = false;
         return acc;
       }, {}),
       corruptionOverride: {
         type: 'none',
         mode: CONST.ACTIVE_EFFECT_MODES.CUSTOM, //custom = use stock items values (i.e. "none")
         value: '0',
-      }
-    }
+      },
+    };
 
     const root = `flags.${COMMON.DATA.name}`;
 
@@ -289,23 +313,21 @@ export class SYB5E {
       sybSoulless: {
         dataPath: `flags.dnd5e.sybSoulless`,
         scope: 'dnd5e',
-        key: 'sybSoulless'
-      }
-    }
+        key: 'sybSoulless',
+      },
+    };
 
     globalThis.game.syb5e.CONFIG.SPELL_PROGRESSION = {
-      none: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-      full: [0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,9,9],
-      half: [0,1,1,2,2,2,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4]
-    }
+      none: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      full: [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9],
+      half: [0, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+    };
 
     CONFIG.DND5E.characterFlags.sybSoulless = {
       hint: 'SYB5E.Sheet.Soulless.Hint',
       name: 'SYB5E.Sheet.Soulless.Name',
       section: 'DND5E.RacialTraits',
-      type: Boolean
-    }
-
+      type: Boolean,
+    };
   }
 }
-
