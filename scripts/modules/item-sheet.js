d8ae3823499184e045af6be12fa63a0b12f3c62c
trioderegion <matt.haentschke@gmail.com>
2023-09-14 12:02:35 -0400

    Fixed approach replacement for subclass

--- scripts/modules/item-sheet.js
+++ scripts/modules/item-sheet.js
@@ -72,10 +72,10 @@ export class Syb5eItemSheet {
     }
 
     /* need to rename "subclass" to "approach" */
-    if (item.type == 'class') {
+    if (item.type == 'subclass') {
 
       /* get the subclass text field entry */
-      const subclassLabel = html.find('[name="system.subclass"]').parent().prev('label');
+      const subclassLabel = html.find('.header-details .item-type');
       if (subclassLabel.length > 0) {
         subclassLabel.text(COMMON.localize("SYB5E.Item.Class.Approach"));
       } else {
